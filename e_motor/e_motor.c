ad985a8d4da683fa510fce1adf41ff0ad1328cb1
AeriCiaff <1723846733@qq.com>
2024-05-18 21:09:25 +0800

    改动部分文件，使得函数作用直观

--- e_motor/e_motor.c
+++ e_motor/e_motor.c
@@ -1,30 +1,15 @@
 #include "main.h"
 #include "tim.h"
 
-/*
-	* @:无刷电机初始化
-	* @:Motor_Init(&htim1, TIM_CHANNEL_1)
-	* @:&htimx:选择使用的定时器
-	* @:TIM_CHANNEL_1:选择使用的通道
-*/
+
 void Motor_Init(TIM_HandleTypeDef *htim, uint32_t Channel)
 {
-	// 初始化电调
 	__HAL_TIM_SetCompare(htim, Channel, 2000);
 	HAL_Delay(4000);
 	__HAL_TIM_SetCompare(htim, Channel, 1000);
 	HAL_Delay(4000);
 }
 
-
-
-/*
-	* @:无刷电机转速控制
-	* @:Motor_Speed(&htim1, TIM_CHANNEL_1, 1500)
-	* @:&htimx:选择使用的定时器
-	* @:TIM_CHANNEL_1:选择使用的通道
-	* @:1500:选择占空比(当频率为50Hz时，范围时1ms~2ms,其中1ms为停止，2ms为满速)
-*/
 void Motor_Speed(TIM_HandleTypeDef *htim, uint32_t Channel, uint16_t Speed)
 {
 	__HAL_TIM_SetCompare(htim, Channel, Speed);
--- e_motor/e_motor.h
+++ e_motor/e_motor.h
@@ -1,12 +1,24 @@
 #ifndef __E_MOTOR_H
 #define __E_MOTOR_H
 
-#include "tim.h" // 确保包含了定时器的头文件
+#include "tim.h"
 
-// 无刷电机初始化函数原型
+/*
+	* @:无刷电机(电调)初始化
+	* @:Motor_Init(&htim1, TIM_CHANNEL_1)
+	* @:&htimx:选择使用的定时器
+	* @:TIM_CHANNEL_1:选择使用的通道
+*/
 void Motor_Init(TIM_HandleTypeDef *htim, uint32_t Channel);
 
-// 无刷电机转速控制函数原型
+
+/*
+	* @:无刷电机转速控制
+	* @:Motor_Speed(&htim1, TIM_CHANNEL_1, 1500)
+	* @:&htimx:选择使用的定时器
+	* @:TIM_CHANNEL_1:选择使用的通道
+	* @:1500:选择占空比(当频率为50Hz时，范围时1ms~2ms,其中1ms为停止，2ms为满速)
+*/
 void Motor_Speed(TIM_HandleTypeDef *htim, uint32_t Channel, uint16_t Speed);
 
-#endif // __MOTOR_H
+#endif
